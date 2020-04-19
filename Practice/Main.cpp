bb08f1685726786de37be0a2cc7873d9a04d6cea
sMeDDveD <dsoloduha@gmail.com>
2020-04-19 11:49:57 +0300

    Implement report

--- Practice/Main.cpp
+++ Practice/Main.cpp
@@ -15,7 +15,7 @@
 #include "GMRES.h"
 #include "ArnoldiGMRES.h"
 
-constexpr double EPS = 0.00000000001;
+constexpr double EPS = 0.000001;
 constexpr double W = 10.0 / 6;
 
 double timeit(Matrix A, Vector b, Vector x) {
@@ -108,7 +108,67 @@ std::pair<double, double> task6(const Matrix& A, const Vector& b, const Vector& 
     return {norm, t};
 }
 
-void cycle(int count = 100, int n = 256) {
+std::pair<double, double> task7(Matrix A, Vector b, const Vector &x)
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+    start = std::chrono::high_resolution_clock::now();
+    auto norm = Utils::CubicNorm(x - SolveHouseholder(std::move(A), std::move(b)));
+    end = std::chrono::high_resolution_clock::now();
+
+    auto t = std::chrono::duration_cast<std::chrono::milliseconds>
+            (end - start).count();
+
+    return {norm, t};
+}
+
+std::pair<double, double> task8(Matrix A, Vector b, const Vector &x)
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+    auto copyA = A;
+    auto copyB = b;
+
+    start = std::chrono::high_resolution_clock::now();
+    auto norm = Utils::CubicNorm(copyA * SolveLeastSquares(std::move(A), std::move(b), true) - copyB);
+    end = std::chrono::high_resolution_clock::now();
+
+    auto t = std::chrono::duration_cast<std::chrono::milliseconds>
+            (end - start).count();
+
+    return {norm, t};
+}
+
+std::pair<double, double> task9(Matrix A, Vector b, const Vector &x)
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+    start = std::chrono::high_resolution_clock::now();
+    auto norm = Utils::CubicNorm(x - SolveGMRES(std::move(A), std::move(b), EPS));
+    end = std::chrono::high_resolution_clock::now();
+
+    auto t = std::chrono::duration_cast<std::chrono::milliseconds>
+            (end - start).count();
+
+    return {norm, t};
+}
+
+std::pair<double, double> task10(Matrix A, Vector b, const Vector &x)
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+    start = std::chrono::high_resolution_clock::now();
+    auto norm = Utils::CubicNorm(x - SolveArnoldiGMRES(std::move(A), std::move(b), EPS));
+    end = std::chrono::high_resolution_clock::now();
+
+    auto t = std::chrono::duration_cast<std::chrono::milliseconds>
+            (end - start).count();
+
+    return {norm, t};
+}
+
+void cycle(int count = 100, int n = 256)
+{
     double minCond, maxCond, avgCond;
     double invTime = 0;
     minCond = std::numeric_limits<double>::max();
@@ -141,13 +201,38 @@ void cycle(int count = 100, int n = 256) {
     maxRelaxationN = std::numeric_limits<double>::min();
     avgRelaxationN = 0;
 
-    for (int i = 0; i < count; i++) {
+    double minHouseHolderN, maxHouseHolderN, avgHouseHolderN;
+    double houseHolderTime = 0;
+    minHouseHolderN = std::numeric_limits<double>::max();
+    maxHouseHolderN = std::numeric_limits<double>::min();
+    avgHouseHolderN = 0;
+
+    double minLeastSquaresN, maxLeastSquaresN, avgLeastSquaresN;
+    double LeastSquaresTime = 0;
+    minLeastSquaresN = std::numeric_limits<double>::max();
+    maxLeastSquaresN = std::numeric_limits<double>::min();
+    avgLeastSquaresN = 0;
+
+    double minGMRESN, maxGMRESN, avgGMRESN;
+    double timeGMRES = 0;
+    minGMRESN = std::numeric_limits<double>::max();
+    maxGMRESN = std::numeric_limits<double>::min();
+    avgGMRESN = 0;
+
+    double minArnoldiGMRESN, maxArnoldiGMRESN, avgArnoldiGMRESN;
+    double timeArnoldiGMRES = 0;
+    minArnoldiGMRESN = std::numeric_limits<double>::max();
+    maxArnoldiGMRESN = std::numeric_limits<double>::min();
+    avgArnoldiGMRESN = 0;
+
+    for (int i = 0; i < count; i++)
+    {
         Matrix A = Matrix::GenerateMatrix(n, Matrix::variant);
         Vector x(n);
         std::iota(x.begin(), x.end(), 1);
         Vector b = A * x;
 
-        auto [condition, t2] = task2(A);
+        auto[condition, t2] = task2(A);
         minCond = std::min(condition, minCond);
         maxCond = std::max(condition, maxCond);
         avgCond += condition;
@@ -173,11 +258,35 @@ void cycle(int count = 100, int n = 256) {
         buildTimeCholesky += t5Build;
         solveTimeCholesky += t5Solve;
 
-        auto [norm6, t6] = task6(A, b, x);
+        auto[norm6, t6] = task6(A, b, x);
         minRelaxationN = std::min(norm6, minRelaxationN);
         maxRelaxationN = std::max(norm6, maxRelaxationN);
         avgRelaxationN += norm6;
         relaxationTime += t6;
+
+        auto[norm7, t7] = task7(A, b, x);
+        minHouseHolderN = std::min(norm7, minHouseHolderN);
+        maxHouseHolderN = std::max(norm7, maxHouseHolderN);
+        avgHouseHolderN += norm7;
+        houseHolderTime += t7;
+
+        auto[norm8, t8] = task8(A.GetSubMatrix(n, n / 2), b, x);
+        minLeastSquaresN = std::min(norm8, minLeastSquaresN);
+        maxLeastSquaresN = std::max(norm8, maxLeastSquaresN);
+        avgLeastSquaresN += norm8;
+        LeastSquaresTime += t8;
+
+        auto[norm9, t9] = task9(A, b, x);
+        minGMRESN = std::min(norm9, minGMRESN);
+        maxGMRESN = std::max(norm9, maxGMRESN);
+        avgGMRESN += norm9;
+        timeGMRES += t9;
+
+        auto[norm10, t10] = task10(A, b, x);
+        minArnoldiGMRESN = std::min(norm10, minArnoldiGMRESN);
+        maxArnoldiGMRESN = std::max(norm10, maxArnoldiGMRESN);
+        avgArnoldiGMRESN += norm10;
+        timeArnoldiGMRES += t10;
 
     }
 
@@ -216,6 +325,34 @@ void cycle(int count = 100, int n = 256) {
     std::cout << "Max norm: " << maxRelaxationN << std::endl;
     std::cout << "Avg norm: " << avgRelaxationN / count << std::endl;
     std::cout << "Avg time of Relaxation: " << relaxationTime / count << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "T7 - Householder: " << std::endl;
+    std::cout << "Min norm: " << minHouseHolderN << std::endl;
+    std::cout << "Max norm: " << maxHouseHolderN << std::endl;
+    std::cout << "Avg norm: " << avgHouseHolderN / count << std::endl;
+    std::cout << "Avg time of Householder: " << houseHolderTime / count << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "T8 - LeastSquares (QR): " << std::endl;
+    std::cout << "Min norm: " << minLeastSquaresN << std::endl;
+    std::cout << "Max norm: " << maxLeastSquaresN << std::endl;
+    std::cout << "Avg norm: " << avgLeastSquaresN / count << std::endl;
+    std::cout << "Avg time of LeastSquares: " << LeastSquaresTime / count << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "T9 - GMRES: " << std::endl;
+    std::cout << "Min norm: " << minGMRESN << std::endl;
+    std::cout << "Max norm: " << maxGMRESN << std::endl;
+    std::cout << "Avg norm: " << avgGMRESN / count << std::endl;
+    std::cout << "Avg time of GMRES: " << timeGMRES / count << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "T10 - GMRES (Arnoldi): " << std::endl;
+    std::cout << "Min norm: " << minArnoldiGMRESN << std::endl;
+    std::cout << "Max norm: " << maxArnoldiGMRESN << std::endl;
+    std::cout << "Avg norm: " << avgArnoldiGMRESN / count << std::endl;
+    std::cout << "Avg time of GMRES (Arnoldi): " << timeArnoldiGMRES / count << std::endl;
     std::cout << std::endl;
 
 
@@ -273,19 +410,19 @@ int main()
 {
     int n = 256;
     double arr[] = {
-			5,  10,   3,
-            -10,  15, - 4,
-              5,   8,  16
+            5, 10, 3,
+            -10, 15, -4,
+            5, 8, 16
     };
     Matrix A = Matrix::GenerateMatrix(n, Matrix::variant);
     Vector x(A.GetCols());
     std::iota(x.begin(), x.end(), 1);
     Vector b = A * x;
 
-	Matrix testMatrix = Matrix::FromArray(arr, 3, 3);
+    Matrix testMatrix = Matrix::FromArray(arr, 3, 3);
 
-	cycle(50, 256);
+    cycle(10, 100);
     //tests(A, b, x);
-	system("pause");
+    system("pause");
     return 0;
 }
